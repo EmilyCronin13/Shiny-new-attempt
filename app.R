ca515b898401749bd68b2086351121f89f7acd63
Your Name <E.cronin15@universityofgalway.ie>
2024-12-22 20:11:55 +0000

    adding more :)

--- app.R
+++ app.R
@@ -52,4 +52,28 @@ padding: 50px;}
 .circle-image { width: 350px;height: 350px;object-fit: cover;border-radius: 50%;}"))),
                                                       
                                                       
+                                                      div( 
+                                                        class = "about-page",
+                                                        div(
+                                                          class = "about-header",
+                                                          img(src = "https://media.giphy.com/media/ToMjGpAibV4AwKryEXS/giphy.gif"),
+                                                          h1("Introduction to the Digitalis Investigation Group (DIG) Trial"),
+                                                          img(src = "https://media.giphy.com/media/l3YSj6Oirgkb18AkE/giphy.gif")
+                                                        ),
+                                                        div(
+                                                          class = "intro-box",
+                                                          p("The Digitalis Investigation Group (DIG) trial was the first large simple trial conducted by the National Heart, Lung, and Blood Institute in conjunction with the Department of Veterans Affair."),
+                                                          p("The Digitalis Investigation Group (DIG) study was a prospective, randomized clinical trial involving 7,788 patients with HF randomized to digoxin or placebo and followed for an average of 37 months."),
+                                                          p("Interactions between age and the following clinical outcomes were examined: total mortality, all-cause hospitalizations, HF hospitalizations, the composite of HF death or HF hospitalizations, hospitalization for suspected digoxin toxicity and withdrawal from therapy because of side effects."),
+                                                          p("A total of 302 centers in the United States and Canada enrolled 7,788 patients between February 1991 and September 1993.")
+                                                        ),
+                                                        div(
+                                                          class = "image-container",
+                                                          img(src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExazBtamw1NWtjeHVoa2Z1ejJybGxyMnQ2czhjejVrNjRxcXYyZHR0NyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3o85xm0pDVY4EkKdFe/giphy.gif", 
+                                                              class = "circle-image")
+                                                        )
+                                                      )
+  )
+  ),
+  
                                                       
